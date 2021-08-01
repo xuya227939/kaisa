38c864dd8ac9e3a1be39f6b023c912f0b9b72d11
xuya227939 <hezhiqianye@gmail.com>
2021-08-01 20:08:57 +0800

    feat: add eslint prettier stylelint

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from 'react-native-screens/native-stack';
-import {enableScreens} from 'react-native-screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import { enableScreens } from 'react-native-screens';
 
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {Provider} from 'mobx-react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Provider } from 'mobx-react';
 import RootStore from './mobx';
 
 enableScreens();
@@ -20,50 +20,42 @@ import My from './pages/my/index';
 import WhiteBoard from './pages/whiteBoard/index';
 
 function HomeStackScreen() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="home" options={{title: '首页'}} component={List} />
-      <Stack.Screen
-        name="details"
-        options={{title: '详情页'}}
-        component={Details}
-      />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="home" options={{ title: '首页' }} component={List} />
+            <Stack.Screen name="details" options={{ title: '详情页' }} component={Details} />
+        </Stack.Navigator>
+    );
 }
 
 function MyScreen() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="my" options={{title: '我的'}} component={My} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="my" options={{ title: '我的' }} component={My} />
+        </Stack.Navigator>
+    );
 }
 
 function WhiteBoardScreen() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="whiteBoard"
-        options={{title: '白板'}}
-        component={WhiteBoard}
-      />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="whiteBoard" options={{ title: '白板' }} component={WhiteBoard} />
+        </Stack.Navigator>
+    );
 }
 
 function App() {
-  return (
-    <Provider rootStore={RootStore}>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="首页" component={HomeStackScreen} />
-          <Tab.Screen name="白板" component={WhiteBoardScreen} />
-          <Tab.Screen name="我的" component={MyScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+    return (
+        <Provider rootStore={RootStore}>
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name="首页" component={HomeStackScreen} />
+                    <Tab.Screen name="白板" component={WhiteBoardScreen} />
+                    <Tab.Screen name="我的" component={MyScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
 }
 
 export default App;
--- pages/details/index.tsx
+++ pages/details/index.tsx
@@ -1,153 +1,157 @@
 import React from 'react';
 import {
-  Button,
-  Text,
-  View,
-  TextInput,
-  Switch,
-  TouchableHighlight,
-  Modal,
-  Alert,
-  StyleSheet,
-  Image,
+    Button,
+    Text,
+    View,
+    TextInput,
+    Switch,
+    TouchableHighlight,
+    Modal,
+    Alert,
+    StyleSheet,
+    Image
 } from 'react-native';
-import {observer, inject} from 'mobx-react';
-import {MapView} from 'react-native-amap3d';
+import { observer, inject } from 'mobx-react';
+import { MapView } from 'react-native-amap3d';
 
 @inject('rootStore')
 @observer
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEnabled: false,
-      modalVisible: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEnabled: false,
+            modalVisible: false
+        };
+    }
+
+    toggleSwitch = () => {
+        this.setState({ isEnabled: !this.state.isEnabled });
     };
-  }
 
-  toggleSwitch = () => {
-    this.setState({isEnabled: !this.state.isEnabled});
-  };
+    setModalVisible = visible => {
+        this.setState({ modalVisible: visible });
+    };
 
-  setModalVisible = visible => {
-    this.setState({modalVisible: visible});
-  };
+    render() {
+        const {
+            rootStore: { count, add, reduce }
+        } = this.props;
 
-  render() {
-    const {
-      rootStore: {count, add, reduce},
-    } = this.props;
+        const { modalVisible } = this.state;
 
-    const {modalVisible} = this.state;
+        return (
+            <View>
+                <View
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <Text>{count}</Text>
+                    <Button onPress={add} title="+" />
+                    <Button onPress={reduce} title="-" />
+                </View>
 
-    return (
-      <View>
-        <View
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text>{count}</Text>
-          <Button onPress={add} title="+" />
-          <Button onPress={reduce} title="-" />
-        </View>
+                <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }} />
+                <Switch
+                    trackColor={{ false: '#767577', true: '#81b0ff' }}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={this.toggleSwitch}
+                    value={this.state.isEnabled}
+                />
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                        this.setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>Hello World!</Text>
 
-        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} />
-        <Switch
-          trackColor={{false: '#767577', true: '#81b0ff'}}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={this.toggleSwitch}
-          value={this.state.isEnabled}
-        />
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-            this.setModalVisible(!modalVisible);
-          }}>
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>Hello World!</Text>
-
-              <TouchableHighlight
-                style={{...styles.openButton, backgroundColor: '#2196F3'}}
-                onPress={() => {
-                  this.setModalVisible(!modalVisible);
-                }}>
-                <Text style={styles.textStyle}>Hide Modal</Text>
-              </TouchableHighlight>
+                            <TouchableHighlight
+                                style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                                onPress={() => {
+                                    this.setModalVisible(!modalVisible);
+                                }}
+                            >
+                                <Text style={styles.textStyle}>Hide Modal</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </Modal>
+                <TouchableHighlight
+                    style={styles.openButton}
+                    onPress={() => {
+                        this.setModalVisible(true);
+                    }}
+                >
+                    <Text style={styles.textStyle}>Show Modal</Text>
+                </TouchableHighlight>
+                <Image
+                    style={styles.tinyLogo}
+                    source={{
+                        uri: 'https://sight-world.oss-cn-hangzhou.aliyuncs.com/images/logo.png'
+                    }}
+                />
+                <MapView
+                    center={{
+                        latitude: 39.91095,
+                        longitude: 116.37296
+                    }}
+                    style={{ height: 300 }}
+                />
             </View>
-          </View>
-        </Modal>
-        <TouchableHighlight
-          style={styles.openButton}
-          onPress={() => {
-            this.setModalVisible(true);
-          }}>
-          <Text style={styles.textStyle}>Show Modal</Text>
-        </TouchableHighlight>
-        <Image
-          style={styles.tinyLogo}
-          source={{
-            uri: 'https://sight-world.oss-cn-hangzhou.aliyuncs.com/images/logo.png',
-          }}
-        />
-        <MapView
-          center={{
-            latitude: 39.91095,
-            longitude: 116.37296,
-          }}
-          style={{height: 300}}
-        />
-      </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    openButton: {
+        backgroundColor: '#F194FF',
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center'
+    },
+    tinyLogo: {
+        width: 50,
+        height: 50
+    }
 });
 
 export default Details;
